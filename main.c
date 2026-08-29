795c4a0ed1500e2010b2a2fda233c670af045a54
haeenah <haeenah629@sookmyung.ac.kr>
2026-08-29 16:14:11 +0900

    실습 2 완료

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    char code1 = 'A';
-    int code2 = 65;
+    int i = 0;
+    char str[4];
+    
+    str[0] = 'a';
+    str[1] = 'b';
+    str[2] = 'c';
+    str[3] = '\0';
 
-    printf("code1 = %c, (%d)\n", code1, code1);
-    printf("code2 = %c, (%d)\n", code2, code2);
+    // %s로 한 번에 출력
+    printf("%s\n", str);
+
+    // while문으로 한 글자씩 출력
+    while (str[i] != '\0') {
+        printf("%c", str[i]);
+        i++;
+    }
+    printf("\n");
 
     return 0;
 }
